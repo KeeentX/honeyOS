5302c9e60e53f172a271a0726a0a57958bf3ce66
John Elias Mojado <121659441+EliasMojado@users.noreply.github.com>
2024-03-19 19:34:38 +0800

    terminal ok now

--- app/desktop/page.tsx
+++ app/desktop/page.tsx
@@ -7,7 +7,7 @@ import Terminal from "./terminal";
 export default function Desktop() {
     const router = useRouter();
     return (
-        <div className="w-[100vw] h-[100vh] relative">
+        <div className="w-[100vw] h-[100vh] relative overflow-hidden">
             <div className="absolute w-full h-full -z-100">
                 <Image 
                     src={'/wallpaper.png'} 
@@ -17,7 +17,7 @@ export default function Desktop() {
                     alt="wallpaper" 
                 />
             </div>
-            {/* <Terminal/> */}
+            <Terminal/>
             <button onClick={router.back}>
                 Logoff
             </button>
--- app/desktop/terminal.tsx
+++ app/desktop/terminal.tsx
@@ -1,11 +1,12 @@
 export default function Terminal() {
     return (
-        <div className=" mt-20 ml-20 text-white">
-            <div className="blur-none">
-            Terminal
+        <div className="relative text-white ml-[5vw] mt-[5vh] w-[50vw]">
+            <div className="absolute w-[45vw] h-[75vh] bg-black/40 blur-none backdrop-blur-sm top-0 -z-100">
+                
             </div>
-            <div className=" w-full h-full bg-black/60 blur-sm backdrop-blur-sm top-0 -z-10">
-            </div>
+            <div className="relative blur-none relative z-20 p-[2vh] mr-[5vw] break-words ">
+                Speak
+            </div> 
         </div>
-    )
+    );    
 }
