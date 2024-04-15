933e6f18a1fc6af1121c7b6f7173451473e6b042
Kent Dave Catipay <kpcatipay@up.edu.ph>
2024-04-15 08:14:55 +0800

    refactor

--- app/program/settings.tsx
+++ app/program/settings.tsx
@@ -1,13 +1,14 @@
 import WindowScreen from "../desktop/components/window";
-import React from "react";
+import React, {useEffect} from "react";
+import {WindowProps} from "@/app/types";
+import {FaGear} from "react-icons/fa6";
 
-export default function Settings({windowIndex, openedWindows, setOpenedWindows}: {
-    windowIndex: number,
-    openedWindows: React.JSX.Element[],
-    setOpenedWindows: React.Dispatch<React.JSX.Element[]>
-}) {
+export default function Settings({windowIndex}: WindowProps) {
     return (
-        <WindowScreen name="SETTINGS" setOpenedWindows={setOpenedWindows} openedWindows={openedWindows} windowIndex={windowIndex}>
+        <WindowScreen name="Settings"
+                      windowIndex={windowIndex}
+                      icon={ <FaGear size={25} color={'yellow'}/>}
+        >
             <div className="p-4">
                 This is the settings
             </div>
