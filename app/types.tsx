2a2393af2354ebc78adc7b6044c8db4d50063b5c
Kent Dave Catipay <kpcatipay@up.edu.ph>
2024-04-15 08:17:10 +0800

    modify WindowProps

--- app/types.tsx
+++ app/types.tsx
@@ -7,21 +7,22 @@ export type FileProps = {
     is_dir: boolean
 }
 
-export type appOpenedProps = {
-    note: number,
-    settings: number,
-    camera: number,
-    fileManager: number
-}
+export type OpenedWindowsProps = {
+    openedWindows: React.JSX.Element[],
+    setOpenedWindows: Dispatch<React.JSX.Element[]>
 
+}
 export type WindowProps = {
-    name?: string;
-    children?: React.ReactNode;
-    setOpenedWindows:  Dispatch<React.JSX.Element[]>;
-    openedWindows: React.JSX.Element[];
-    setAppOpenedState?:  Dispatch<appOpenedProps>;
-    appOpenedState: appOpenedProps;
-    windowIndex?: number;
+    name?: string,
+    customName?: string,
+    children?: React.ReactNode,
+    icon?: React.JSX.Element,
+    windowIndex?: number,
+    file?: {
+        content: string,
+        location: string
+        name: string,
+    }
 }
 
 export type HoneyFile = {
