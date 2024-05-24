be1e02fa6daee02de189614805579c38c9a78680
Kent Dave Catipay <kpcatipay@up.edu.ph>
2024-05-24 20:28:21 +0800

    initial running process highlighting

--- app/desktop/manager.tsx
+++ app/desktop/manager.tsx
@@ -4,35 +4,15 @@ import { Process, SchedulerContext } from "../context/schedulerContext";
 import { SchedulerProviderProps } from "../context/schedulerContext";
 
 export default function Manager (){
-    const {openedWindows} = useContext(OpenedWindowsContext);
-    const {processes, setProcesses, fifo} = useContext(SchedulerContext);
-    const [schedulerSet, setSchedulerSet] = useState(false);
+    const {openedWindows, numberOfOpenedWindows} = useContext(OpenedWindowsContext);
+    const {processes, setProcesses, FIFO} = useContext(SchedulerContext);
 
     useEffect(() => {
-        setProcesses(() => {
-            const newProcesses : Process[] = [];
-            openedWindows.map((window, index) => {
-                window.html && newProcesses.push({
-                    name: window.name,
-                    status: 0,
-                    bursttime: processes[index] && processes[index].bursttime ? processes[index].bursttime : Math.ceil((Math.random() * 10 % 3)),
-                    arrivaltime: 0,
-                    priority: 0,
-                    memory: processes[index] && processes[index].memory ? processes[index].memory: Math.ceil((Math.random() * 1000)),
-                })
-            })
+        if(processes.length === 0) return;
+        const timer = FIFO(processes, -1, 0.5);
 
-            return newProcesses;
-        })
-        
-    },[openedWindows])
-
-    useEffect(() =>{
-        if(!schedulerSet && processes.length) {
-            fifo();
-            setSchedulerSet(true); 
-        }
-    }, [processes])
+        return () => clearTimeout(timer);
+    }, [numberOfOpenedWindows])
 
     return (
         <div className="font-consolas relative text-white ml-[5vw] mt-[5vh] w-[50vw]">
@@ -51,17 +31,19 @@ export default function Manager (){
                         <td>Status</td>
                     </tr>
                     {processes.map((process: Process, index: number) => {
-                    return <tr key={index}>
-                    <td>{process.priority}</td>
-                    <td>{process.name}</td>
-                    <td>{process.bursttime}</td>
-                    <td>{process.memory} kb</td>
-                    <td>{process.arrivaltime}</td>
-                    <td>{process.status}</td>
-                    </tr>
-                })}
+                    return (
+                        <tr key={index} className={`${process.status === 2 && 'bg-green-500'}`}>
+                            <td>{process.priority}</td>
+                            <td>{process.name}</td>
+                            <td>{process.burstTime}</td>
+                            <td>{process.memory} kb</td>
+                            <td>{process.arrivalTime}</td>
+                            <td>{process.status}</td>
+                        </tr>
+                    )
+                    })}
                 </table>
-                
+
             </div>
         </div>
     )
