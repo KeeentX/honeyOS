3f2839859c7e06c90fa42f6ca8dc6d0ce03a2670
John Elias Mojado <121659441+EliasMojado@users.noreply.github.com>
2024-05-27 12:02:20 +0800

    virtual memory

--- app/desktop/voice.tsx
+++ app/desktop/voice.tsx
@@ -1,79 +1,146 @@
 import {useEffect, useState, useRef, useContext} from 'react';
-import {SpeechRecognitionContext} from "@/app/context/speechRecognitionContext";
+import { Process, SchedulerContext } from "../context/schedulerContext";
+
+const colorMap = {
+    "Note": "#3B4163",
+    "Settings" : "#346E82",
+    "Camera": "#FFF5D1",
+    "File Manager": "#F7CE72",
+    "Spotify": "#F77269",
+    "Chess": "#CF4F44"
+}
 
 export default function Voice() {
-    const {payload, audioContextRef} = useContext(SpeechRecognitionContext);
-    const analyserRef = useRef<AnalyserNode | null>(null);
-    const dataArrayRef = useRef<Uint8Array | null>(null);
-    const [microphoneName, setMicrophoneName] = useState<string>('Unknown');
-    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const {
+        readyProcesses,
+        waitProcesses,       
+    } = useContext(SchedulerContext);
 
-    // useEffect(() => {
-    //     navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-    //         const audioContext = new AudioContext();
-    //         audioContextRef.current = audioContext;
-    //         const analyser = audioContext.createAnalyser();
-    //         analyserRef.current = analyser;
-    //         const source = audioContext.createMediaStreamSource(stream);
-    //         source.connect(analyser);
-    //         dataArrayRef.current = new Uint8Array(analyser.frequencyBinCount);
-    //
-    //         // MIRCOPHONE NAME
-    //         const audioInputDevices = stream.getAudioTracks().map((track) => track.label);
-    //         if (audioInputDevices.length > 0) {
-    //             setMicrophoneName(audioInputDevices[0]);
-    //         }
-    //
-    //         // SOUND WAVE
-    //         const canvas = canvasRef.current;
-    //         if (canvas) {
-    //             const canvasCtx = canvas.getContext('2d');
-    //             if (canvasCtx) {
-    //                 const draw = () => {
-    //                     if (analyser && dataArrayRef.current) {
-    //                         analyser.getByteTimeDomainData(dataArrayRef.current);
-    //                         canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-    //                         canvasCtx.fillStyle = 'rgb(0, 0, 0, 0)';
-    //                         canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-    //                         canvasCtx.lineWidth = 3;
-    //                         canvasCtx.strokeStyle = 'rgb(255, 255, 255)';
-    //                         canvasCtx.beginPath();
-    //                         const sliceWidth = canvas.width / analyser.frequencyBinCount;
-    //                         let x = 0;
-    //                         for (let i = 0; i < analyser.frequencyBinCount; i++) {
-    //                             const v = dataArrayRef.current[i] / 128.0;
-    //                             const y = v * canvas.height / 2;
-    //                             if (i === 0) {
-    //                                 canvasCtx.moveTo(x, y);
-    //                             } else {
-    //                                 canvasCtx.lineTo(x, y);
-    //                             }
-    //                             x += sliceWidth;
-    //                         }
-    //                         canvasCtx.lineTo(canvas.width, canvas.height / 2);
-    //                         canvasCtx.stroke();
-    //                     }
-    //                     requestAnimationFrame(draw);
-    //                 };
-    //                 draw();
-    //             }
-    //         }
-    //     });
-    // }, []);
+    const [allProcesses, setAllProcesses] = useState<Process[]>([]);
+    const [history, setHistory] = useState<String[]>([]);
+    const [memoryMap, setMemoryMap] = useState<String[]>(new Array(20).fill(''));
+
+    useEffect(() => {
+        setAllProcesses([...readyProcesses, ...waitProcesses]);
+    }, [readyProcesses, waitProcesses]);
+    
+    // HISTORY
+    useEffect(() => {
+        // Iterate through the readyProcesses
+        readyProcesses.forEach((process: Process) => {
+            // If the process is RUNNING and burstTime is greater than 0
+            if(process.status === 2 && process.burstTime > 0) {
+                // Add the process to the history
+                if(!history.includes(process.name)){
+                    setHistory((prev) => [...prev, process.name]);
+                }else{
+                    // If the process is already in the history, remove it
+                    setHistory((prev) => prev.filter((name) => name !== process.name));
+                    setHistory((prev) => [...prev, process.name]);
+                }
+            }
+        })
+        
+        let allProcesses = readyProcesses.concat(waitProcesses);
+
+        // Iterate through the history
+        history.forEach((processName: String) => {
+            // if the process is not in the readyProcesses or waitProcesses
+            if(!allProcesses.some((process: Process) => process.name === processName)){
+                // Remove the process from the history
+                setHistory((prev) => prev.filter((name) => name !== processName));
+            }
+        })
+
+    }, [allProcesses])
+
+    // MEMORY MAP
+    useEffect(() => {
+        // Create a copy of memoryMap
+        let updatedMemoryMap = [...memoryMap];
+
+        // Iterate through the memoryMap
+        updatedMemoryMap.forEach((page: String, index: number) => {
+            // If the page is not in the allProcesses
+            if(page != '' && !allProcesses.some((process: Process) => process.name === page)){
+                // Set the page to empty
+                updatedMemoryMap[index] = '';
+            }
+        })
+
+        // Iterate through allProcesses
+        allProcesses.forEach((process: Process) => {
+            // get number of pages required
+            let requiredNumPages = Math.ceil(process.memory / 50000);
+            // get number of pages loaded
+            let loadedNumPages = updatedMemoryMap.filter((page) => page === process.name).length;
+            // get number of pages needed
+            let neededNumPages = requiredNumPages - loadedNumPages;
+
+            // First pass: loading memory on empty pages
+            if(neededNumPages > 0){
+                for(let i = 0; i < 20; i++){
+                    if(updatedMemoryMap[i] === ''){
+                        updatedMemoryMap[i] = process.name;
+                        neededNumPages--;
+                    }
+                    if(neededNumPages === 0) break;
+                }
+            }
+
+            // If the process is RUNNING and burstTime is greater than 0
+            if (process.status === 2 && process.burstTime > 0) {
+                // Second pass: loading pages on pages with old processes
+                if(neededNumPages > 0){
+                    // get the oldest process in the history
+                    for(let i = 0; i < history.length; i++){
+                        let oldProcessName = history[i];
+
+                        // replace the pages
+                        for(let j = 0; j < 20; j++){
+                            if(updatedMemoryMap[j] === oldProcessName){
+                                updatedMemoryMap[j] = process.name;
+                                neededNumPages--;
+                            }
+                            if(neededNumPages === 0) break;
+                        }
+                        if(neededNumPages === 0) break;
+                    }
+                }
+            }
+        });
+
+        // Update the memoryMap state with the new copy
+        setMemoryMap(updatedMemoryMap);
+
+    }, [allProcesses]);
 
     return (
-        <div className={`font-consolas relative text-white ml-[5vw] mt-[10vh] w-[50vw]absolute w-[40vw] h-[25vh] 
-        bg-black/40 blur-none backdrop-blur-sm top-0 -z-100`}>
-            <div className="blur-none z-20 p-[2vh] mr-[5vw] break-words">
-                MICROPHONE: {microphoneName}
+        <div className="font-consolas relative text-white ml-[5vw] mt-[10vh] absolute w-[40vw] h-[25vh] 
+        bg-black/40 blur-none backdrop-blur-sm top-0 -z-100 flex flex-col justify-between p-[2vh]">
+
+            <div>
+                VIRTUAL MEMORY MANAGEMENT
             </div>
-            <div className={`blur-none z-20 pl-[2vh] mr-[5vw] break-words overflow-x-auto
-             min-h-[4vh] max-h-[4vh] scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent`}>
-                {payload}
+
+            <div>
+                HISTORY: {history.join(' -> ')}
             </div>
-            <div className="blur-none flex justify-center w-[40vw]">
-               <canvas ref={canvasRef} className={`w-full p-1 h-[100px]`}></canvas>
+
+            <div className="flex justify-center">
+                {memoryMap.map((processName, index) => (
+                    <div
+                        key={index}
+                        className="inline-block h-[10vh] w-[1.5vw] mr-[2px]"
+                        style={{
+                            backgroundColor: processName ? colorMap[processName.toString() as keyof typeof colorMap] : 'transparent',
+                            border: '1px solid white'
+                        }}
+                        title={processName ? processName.toString() : 'Empty Page'}
+                    />
+                ))}
             </div>
+
         </div>
     );
 }
